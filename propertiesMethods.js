8559bb5d9c4e5033f12cc9f462bd31f204e691c8
Vincent Dizon <vdizon@cps.edu>
2024-04-04 15:49:53 +0000

    finished tasks

--- propertiesMethods.js
+++ propertiesMethods.js
@@ -1,14 +1,14 @@
 let myArray1 = [1, 2, 3, 4, 5];
 
 // 1. Console log the length of myArray1
-console.log(); // Should print 5
+console.log(myArray1.length); // Should print 5
 
 
 
 
 let myArray2 = [1, 2, 3];
 // 2. Add the number 4 to the end of myArray2
-
+myArray2.push(4);
 
 console.log(myArray2); // Should print [1, 2, 3, 4]
 
@@ -17,6 +17,7 @@ console.log(myArray2); // Should print [1, 2, 3, 4]
 
 let myArray3 = [1, 2, 3, 4];
 // 3. Remove the last element from myArray3
+myArray3.pop();
 
 
 console.log(myArray3); // Should print [1, 2, 3]
@@ -26,6 +27,7 @@ console.log(myArray3); // Should print [1, 2, 3]
 
 let myArray4 = [1, 2, 3, 4];
 // 4. Remove the first element from myArray4
+myArray4.shift();
 
 
 console.log(myArray4); // Should print [2, 3, 4]
@@ -34,7 +36,7 @@ console.log(myArray4); // Should print [2, 3, 4]
 
 let myArray5 = [2, 3, 4];
 // 5. Add the number 1 to the beginning of myArray5
-
+myArray5.unshift(1);
 
 console.log(myArray5); // Should print [1, 2, 3, 4]
 
@@ -44,5 +46,14 @@ console.log(myArray5); // Should print [1, 2, 3, 4]
 // CHALLENGE
 // Look up how a spread operator works.
 //  - Try combing a couple of the arrays above using it.
+function printFirstThree(x,y,z) {
+    console.log("First three: ",x, y, z);
+
+
+}
+
+printFirstThree(...myArray5);
+
+
 
 
--- script.js
+++ script.js
@@ -10,15 +10,15 @@ submit.addEventListener("click", function() {
     // Do Now: Complete tasks 1-5.
 
     if (userInput === "English") {
-        div.innerHTML = ""; // 1. Replace empty string with correct array element
+        div.innerHTML = greetings[0]; // 1. Replace empty string with correct array element
     } else if (userInput === "Spanish") {
-        div.innerHTML = ""; // 2. Replace empty string with correct array element
+        div.innerHTML = greetings[1]; // 2. Replace empty string with correct array element
     } else if (userInput === "French") {
-        div.innerHTML = ""; // 3. Replace empty string with correct array element
+        div.innerHTML = greetings[2]; // 3. Replace empty string with correct array element
     } else if (userInput === "German") {
-        div.innerHTML = ""; // 4. Replace empty string with correct array element
+        div.innerHTML = greetings[3]; // 4. Replace empty string with correct array element
     } else if (userInput === "Japanese") {
-        div.innerHTML = ""; // 5. Replace empty string with correct array element
+        div.innerHTML = greetings[4]; // 5. Replace empty string with correct array element
     } else {
         div.innerHTML = "Apologies, I do not support that language.";
     }
